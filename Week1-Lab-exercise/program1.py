0463cb0bbf7aaaf0eade99c56f24fc89f2cbbbe5
Purna Sai Madala <purna_sai@srmap.edu.in>
2020-08-06 23:04:50 +0530

    Update program1.py
    Program done in Python

--- Week1-Lab-exercise/program1.py
+++ Week1-Lab-exercise/program1.py
@@ -2,3 +2,17 @@
 Question : Implementation of Language recognizer for set of all strings over input alphabet ∑={a,b}
 containing even number of a’s and even number of b’s.
 """
+#Taking the characters to be recognized as 'a' and 'b'.
+inp = input("Enter Input String : ")
+state_a = 0 
+state_b = 0
+for i in range(0,len(inp)):
+    if inp[i] == 'a':
+        state_a += 1    
+    if inp[i] == 'b':
+        state_b += 1
+        
+if state_a%2==0 and state_b%2==0:
+    print("String Accepted")
+else:
+    print("String Invalid")
