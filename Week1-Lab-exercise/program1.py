4faf96f4d1a19980ed2e2f19cd21b240fb013ef7
Purna Sai Madala <purna_sai@srmap.edu.in>
2020-08-16 20:41:51 +0530

    Final Commit

--- Week1-Lab-exercise/program1.py
+++ Week1-Lab-exercise/program1.py
@@ -7,9 +7,9 @@ inp = input("Enter Input String : ")
 state_a = 0 
 state_b = 0
 for i in range(0,len(inp)):
-    if inp[i] == 'a':
+    if inp[i] == 'a' :
         state_a += 1    
-    if inp[i] == 'b':
+    if inp[i] == 'b' :
         state_b += 1
         
 if state_a%2==0 and state_b%2==0:
